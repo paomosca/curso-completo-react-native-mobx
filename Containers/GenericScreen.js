3a96c0363bf23dc1a090d5a05b4afcddaa815853
paolo mosca <pablo80tango@gmail.com>
2018-08-30 09:49:31 +0200

    StyleSheet

--- Containers/GenericScreen.js
+++ Containers/GenericScreen.js
@@ -7,7 +7,7 @@
  */
 
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 /*
 
@@ -24,25 +24,30 @@ export default class GenericScreen extends Component<Props> {
   render() {
     return (
       <View>
-        <View
-          style={{
-            marginTop: 20,
-            height: 56,
-            backgroundColor: "#eeeeee",
-            alignItems: "center",
-            justifyContent: "center"
-          }}
-        >
-          <Text
-            style={{
-              fontWeight: "bold"
-            }}
-          >
-            Title
-          </Text>
+        <View style={styles.navBar}>
+          <Text style={styles.title}>Title</Text>
+        </View>
+        <View style={[styles.navBar, styles.redBar]}>
+          <Text style={styles.title}>Title</Text>
         </View>
         {/* content goes here */}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  navBar: {
+    marginTop: 20,
+    height: 56,
+    backgroundColor: "#eeeeee",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  title: {
+    fontWeight: "bold"
+  },
+  redBar: {
+    backgroundColor: "#ff0000"
+  }
+});
